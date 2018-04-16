0d4ee88660e5e53431067a8bd65db1766f4483ff
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-04-16 15:15:31 +0900

    Update Rt_motor_server.py

--- scripts/Rt_motor_server.py
+++ scripts/Rt_motor_server.py
@@ -5,7 +5,7 @@ from Rt_motor import Rt_motor_driver
 
 class Rt_motor_server:
   def __init__(self):
-    self.motor_driv=Rt_motor_diver()
+    self.motor_driv=Rt_motor_driver()
     self.motor_driv.r_motor(400)
     self.motor_driv.l_motor(400)
     

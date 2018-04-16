0356f20e8a32d8a3bd5468fe6cb2ebcfa1f46505
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-04-16 15:10:34 +0900

    Update Rt_motor_server.py

--- scripts/Rt_motor_server.py
+++ scripts/Rt_motor_server.py
@@ -2,3 +2,15 @@
 import rospy
 import time
 from Rt_motor import Rt_motor_driver
+
+class Rt_motor_server:
+  def __init__(self):
+    self.motor_driv=Rt_motor_diver()
+    self.motor_driv.
+    
+    
+    
+    
+    
+if __name__=='_main__':
+  ins=Rt_motor_server()

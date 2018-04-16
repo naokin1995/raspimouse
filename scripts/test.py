1c0e69d30f1c2fb2a6ce27eda1e211ea9e08add0
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-04-16 13:09:45 +0900

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,26 @@
 import rospy
+import time
 
 
-print 'hello world.'
+class Rt_motor_driver:
+  self.init_file='/dev/rtmotoren0'
+  
+  def __init__(self):
+    __init_motor()
+    
+    
+    
+  def __init_motor(self):
+    try:
+      with open(init_file,'w') as f:
+        print >> f,'1'
+        
+    except:
+      rospy.logger("Cannot write to"+ self.init_file)
+      
+      
+      
+def __name__ =="__main__":
+  ins=Rt_motor_driver()
+      
+      

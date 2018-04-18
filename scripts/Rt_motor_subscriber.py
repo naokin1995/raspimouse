d2e4ae478b683e5f299f3770825ca1ef43023823
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-04-18 16:39:53 +0900

    Update Rt_motor_subscriber.py

--- scripts/Rt_motor_subscriber.py
+++ scripts/Rt_motor_subscriber.py
@@ -3,9 +3,11 @@ import rospy
 import time
 from Rt_motor import Rt_motor_driver
 
-class Rt_motor_server:
+class Rt_Motor_Subscriber:
   def __init__(self):
     self.motor_driv=Rt_motor_driver()
+    rospy.init_node('Rt_motor_Subscriber')
+    
     self.motor_driv.r_motor(400)
     self.motor_driv.l_motor(400)
     
@@ -17,5 +19,7 @@ class Rt_motor_server:
     
     
     
+    
+    
 if __name__=='__main__':
-  ins=Rt_motor_server()
+  ins=Rt_Motor_Subscriber()

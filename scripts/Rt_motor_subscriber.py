62865e8d18009f8ae9d09ec643dc33f32c30572f
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-04-18 17:42:06 +0900

    Update Rt_motor_subscriber.py

--- scripts/Rt_motor_subscriber.py
+++ scripts/Rt_motor_subscriber.py
@@ -2,11 +2,14 @@
 import rospy
 import time
 from Rt_motor import Rt_motor_driver
+from sensor_msg import Joy
+
 
 class Rt_Motor_Subscriber:
   def __init__(self):
     self.motor_driv=Rt_motor_driver()
     rospy.init_node('Rt_motor_Subscriber')
+    self.sub=Subscriber('/joy',Joy,self.callback)
     
     self.motor_driv.r_motor(400)
     self.motor_driv.l_motor(400)
@@ -16,10 +19,9 @@ class Rt_Motor_Subscriber:
     self.motor_driv.r_motor(0)
     self.motor_driv.l_motor(0)
     
-    
-    
-    
-    
-    
+  def callback(message):
+    print message
+
+
 if __name__=='__main__':
   ins=Rt_Motor_Subscriber()

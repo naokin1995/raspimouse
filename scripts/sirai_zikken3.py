648f3976c80c6fd331f5a304f1ac3b61cacabe11
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-18 16:50:46 +0900

    Update sirai_zikken3.py

--- scripts/sirai_zikken3.py
+++ scripts/sirai_zikken3.py
@@ -1,1 +1,13 @@
 
+motor_file='/dev/remotoren0'
+motor_filel='/dev/remotor_raw_l0'
+motor_filer='/dev/remotor_raw_r0'
+
+with open (motor_file,'w') as m:
+ print >> m,'1'
+with open (motor_filel,'w') as ml:
+ print >> ml,'400'
+with open (motor_filer,'w') as mr:
+ print >> mr,'400'
+
+

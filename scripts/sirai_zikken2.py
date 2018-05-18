d26c6e94c579be745268b33c65aa8e8413cb1333
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-18 16:37:48 +0900

    Update sirai_zikken2.py

--- scripts/sirai_zikken2.py
+++ scripts/sirai_zikken2.py
@@ -7,6 +7,13 @@ while True:
   data=se.read()
   list_data=data.split(' ')
   print list_data[0] + ' ' + list_data[3]
-  
+   if (list_data[0] <'200' and list_data[3]<'200'):
+    with open(buzzer_file,'w') as b:
+     print >> b,'10000'
+   else:
+    with open(buzzer_file,'w') as b:
+     print >> b,'0'
+     
+    
   
 

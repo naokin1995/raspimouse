1b32495a9bb9266e9ddbb4b3930aa588de10c3bd
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-18 15:59:35 +0900

    Update sirai_zikken1.py

--- scripts/sirai_zikken1.py
+++ scripts/sirai_zikken1.py
@@ -7,7 +7,7 @@ switch_file='/dev/rtswitch0'
 
 while True:
   with open(switch_file,'r') as s:
-    if (s=='1'):
+    if (s.read()=='1'):
       with open(led_file,'w') as f:
        print >>f,'0'
     else:

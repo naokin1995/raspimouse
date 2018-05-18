ecc675a10b04e7d3ba1f72bd1e50a15967023891
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-18 16:58:36 +0900

    Update sirai_zikken3.py

--- scripts/sirai_zikken3.py
+++ scripts/sirai_zikken3.py
@@ -2,12 +2,25 @@
 motor_file='/dev/rtmotoren0'
 motor_filel='/dev/rtmotor_raw_l0'
 motor_filer='/dev/rtmotor_raw_r0'
+sensor_file='/dev/rtlightsensor0'
 
 with open (motor_file,'w') as m:
  print >> m,'1'
 with open (motor_filel,'w') as ml:
- print >> ml,'400'
-with open (motor_filer,'w') as mr:
- print >> mr,'400'
+ print >> ml,'100'
+with open (motor_filelr,'w') as mr:
+ print >> mr,'100'
+ 
+while True:
+ with open(sensor_file,'r') as se:
+  data=se.read()
+  list_data=data.split(' ')
+  print list_data[0] + ' ' + list_data[3]
+ if (int(list_data[0])>200 and int(list_data[3])>200):
+  with open (motor_filel,'w') as ml:
+   print >> ml,'0'
+  with open (motor_filelr,'w') as mr:
+   print >> mr,'0'
+
 
 

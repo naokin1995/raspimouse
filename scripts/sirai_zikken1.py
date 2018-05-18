5c10715d4d6579972c33704c5f817186801e9510
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-18 15:46:30 +0900

    Update sirai_zikken1.py

--- scripts/sirai_zikken1.py
+++ scripts/sirai_zikken1.py
@@ -2,8 +2,12 @@
 
 
 led_file='/dev/rtled0'
-
-with open(led_file,'w') as f:
-  print >>f,'1'
+switch_file='/dev/rtswitch0'
 
 
+while true:
+  with open(switch_file,'r') as s:
+    if (s==o):
+      with open(led_file,'w') as f:
+       print >>f,'1'
+  

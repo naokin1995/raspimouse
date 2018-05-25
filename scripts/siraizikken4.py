5064f716b1721c8ec2464d3e11945471043ea06a
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-25 15:40:23 +0900

    Update siraizikken4.py

--- scripts/siraizikken4.py
+++ scripts/siraizikken4.py
@@ -2,11 +2,25 @@
 import rospy
 from sensor_msgs.msg import Joy
 
+motor_file='/dev/rtmotoren0'
+motor_filel='/dev/rtmotor_raw_l0'
+motor_filer='/dev/rtmotor_raw_r0'
+with open (motor_file,'w') as m:
+print >> m,'1'
 
 def callback(msg):
-  print msg.axes[0]
-  print msg.axes[1]
-
+   if (axes[0]==1):
+     with open (motor_filel,'w') as ml:
+       print >> ml,'400'
+     with open (motor_filer,'w') as mr:
+       print >> mr,'400'
+   if (axes[0]==-1):
+     with open (motor_filel,'w') as ml:
+       print >> ml,'-400'
+     with open (motor_filer,'w') as mr:
+       print >> mr,'-400'
+  
+  
 
 rospy.init_node("joy_subscriber")
 sub = rospy.Subscriber('joy',Joy,callback,queue_size=1)

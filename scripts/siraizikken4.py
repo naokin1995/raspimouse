a1966ee58fce3720c9f773976674ea7670f722f0
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-05-25 15:28:32 +0900

    Update siraizikken4.py

--- scripts/siraizikken4.py
+++ scripts/siraizikken4.py
@@ -2,9 +2,13 @@
 import rospy
 from sensor_msgs.msg import Joy
 
-rospy.init_node("joy_subscriber")
-sub = rospy.Subscriber('joy',Joy,callback,queue_size=1)
 
 def callback(msg):
   print msg
+
+
+rospy.init_node("joy_subscriber")
+sub = rospy.Subscriber('joy',Joy,callback,queue_size=1)
+
+
 

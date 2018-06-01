f2a59aea8a9293c19a979df840a456c4bb16bcae
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-06-01 16:02:28 +0900

    Update sirai_zikken6.py

--- scripts/sirai_zikken6.py
+++ scripts/sirai_zikken6.py
@@ -25,26 +25,24 @@ while(1):
 
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
+    lower_red = np.array([150,100,50])
+    upper_red = np.array([180,255,255])
 
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask_b = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask_r = cv2.inRange(hsv, lower_red, upper_red)
 
-    area=cv2.countNonZero(mask)
-    print area
+    area_b=cv2.countNonZero(mask_b)
+    print area_b
+    area_r=cv2.countNonZero(mask_r)
+    print area_r
     
-    if (area>1000):
+    right_motor(0)
+    left_motor(0)
+    
+    if (area_b>1000):
         right_motor(400)
         left_motor(400)
+    rlif(area_r>1000):
+        right_motor(-400)
+        left_motor(-400)
         
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-    
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    
-    
-    
-    if k == 27:
-        break
-
-cv2.destroyAllWindows()

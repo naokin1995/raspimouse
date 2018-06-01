4c8c37f1a7eeb6663438244af16dcae5d8a67318
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-06-01 16:46:10 +0900

    Update sirai_zikken6.py

--- scripts/sirai_zikken6.py
+++ scripts/sirai_zikken6.py
@@ -27,33 +27,26 @@ while(1):
     upper_blue = np.array([130,255,255])
     lower_red = np.array([150,10,15])
     upper_red = np.array([180,255,255])
-    lower_yellow = np.array([50,100,50])
-    upper_yellow = np.array([100,255,255])
+
+    
 
     mask_b = cv2.inRange(hsv, lower_blue, upper_blue)
     mask_r = cv2.inRange(hsv, lower_red, upper_red)
-    mask_y = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
     area_b=cv2.countNonZero(mask_b)
 
     area_r=cv2.countNonZero(mask_r)
 
-    area_y=cv2.countNonZero(mask_y)
-
     
     right_motor(0)
     left_motor(0)
     
-    if (area_b>area_r and area_b>area_y ):
+    if (area_b>area_r and area_b):
         right_motor(400)
         left_motor(400)
         print 'blue'
-    elif(area_r>area_b and area_r>area_y):
+    elif(area_r>area_b and area_r):
         right_motor(-400)
         left_motor(-400)
         print 'red'
-    elif(area_y>area_b and area_y>area_r):
-        right_motor(0)
-        left_motor(0)
-        print 'yellow'
-        
+

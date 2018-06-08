e66471aded9345453a2ae39856df2b6b519d45ef
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-06-08 16:34:37 +0900

    Update sirai_zikken7.py

--- scripts/sirai_zikken7.py
+++ scripts/sirai_zikken7.py
@@ -24,14 +24,14 @@ motor_file='/dev/rtmotoren0'
 motor_filel='/dev/rtmotor_raw_l0'
 motor_filer='/dev/rtmotor_raw_r0'
 with open (motor_file,'w') as m:
-  print >> (m,'1')
+  m.write('1')
 
 def right_motor(num):
  with open (motor_filer,'w') as mr:
-       print >> mr,str(num)
+  mr.write(str(num))
 def left_motor(num):
  with open (motor_filel,'w') as ml:
-       print >> ml,str(num)
+  ml.write(str(num))
         
 while True:
 

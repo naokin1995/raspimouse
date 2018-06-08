4d9344049047e49705ada6835a2202226d0393d7
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-06-08 16:44:20 +0900

    Update sirai_zikken7.py

--- scripts/sirai_zikken7.py
+++ scripts/sirai_zikken7.py
@@ -44,7 +44,8 @@ while True:
                 if index != -1:
                     line = line[index+6:line.find('"',index+6)]
                     strTemp += str(line)
-                    print (type(strTemp))
+                    word = strTemp.split('')
+                    print (word[0])
                     if(strTemp=='前進'):
                         right_motor(400)
                         left_motor(400)

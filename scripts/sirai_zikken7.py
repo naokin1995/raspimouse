8b528e44f4c56da842c5ce56451102da0660d0c9
Naoki Miyai <33481792+naokin1995@users.noreply.github.com>
2018-06-08 16:30:37 +0900

    Update sirai_zikken7.py

--- scripts/sirai_zikken7.py
+++ scripts/sirai_zikken7.py
@@ -24,7 +24,7 @@ motor_file='/dev/rtmotoren0'
 motor_filel='/dev/rtmotor_raw_l0'
 motor_filer='/dev/rtmotor_raw_r0'
 with open (motor_file,'w') as m:
-  print >> m,'1'
+  print >> (m,'1')
 
 def right_motor(num):
  with open (motor_filer,'w') as mr:
